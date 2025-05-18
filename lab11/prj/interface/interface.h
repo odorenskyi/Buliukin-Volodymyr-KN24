2d23401c9227e522b11d5bd79fefeddce156aeaa
kuroshi_dev <kuroshit23@gmail.com>
2025-05-18 14:40:17 +0300

    Added prj files with teammates functions

---
+++ lab11/prj/interface/interface.h
@@ -0,0 +1,10 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include "..\Modules_Kurishchenko_ShowAndSearch\modules_kurishchenko_showandsearch.h"
+#include "..\Modules_Radomska_AddAndDelete\modules_radomska_addanddelete.h"
+#include "..\Modules_Buliukin_LoadAndSaveDb\modules_buliukin_loadandsavedb.h"
+
+void handleUserChoice(); // Основна функція для вибору операцій
+
+#endif // INTERFACE_H
